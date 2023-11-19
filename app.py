47ac3c6478d3b872599624ce95c18bdfa0d03c62
rahulmr96 <rahulmranga@gmail.com>
2023-11-19 15:26:08 -0500

    Updated port

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def remove_job_api():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5069)
